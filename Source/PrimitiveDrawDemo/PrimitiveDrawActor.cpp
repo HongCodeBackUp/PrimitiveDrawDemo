b8c81017120fca2eb66b26b4e92a8e1e3d9136c9
Hong Gu <macrogu@qq.com>
2023-01-06 00:32:12 +0800

    add component

--- Source/PrimitiveDrawDemo/PrimitiveDrawActor.cpp
+++ Source/PrimitiveDrawDemo/PrimitiveDrawActor.cpp
@@ -10,6 +10,9 @@ APrimitiveDrawActor::APrimitiveDrawActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	PrimitiveDrawComp = CreateDefaultSubobject<UPrimitiveDrawComponent>("PrimitiveDrawComp");
+	RootComponent = PrimitiveDrawComp;
+
 }
 
 // Called when the game starts or when spawned
