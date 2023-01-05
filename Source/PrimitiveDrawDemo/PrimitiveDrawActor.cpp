0f8632d86257cf663401549171d41d0cdc3771b0
Hong Gu <macrogu@qq.com>
2023-01-06 00:00:03 +0800

    add plugin

---
+++ Source/PrimitiveDrawDemo/PrimitiveDrawActor.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2023, Hong Gu <guhong.usa@gmail.com>. All Rights Reserved.
+
+#include "PrimitiveDrawActor.h"
+#include "PrimitiveDrawComponent.h"
+
+
+// Sets default values
+APrimitiveDrawActor::APrimitiveDrawActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void APrimitiveDrawActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
